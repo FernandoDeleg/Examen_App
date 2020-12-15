07dbd348e05943214c44e9bd09eb65283d7a4161
FernandoDeleg <56567621+FernandoDeleg@users.noreply.github.com>
2020-12-14 20:36:17 -0500

    Add files via upload
    carga Inicial (20:35)

---
+++ ExamenDelegFernandoServidor/src/main/java/ec/ups/edu/appdis/g1/ExamenDelegFernandoServidor/modelo/Ticket.java
@@ -0,0 +1,45 @@
+package ec.ups.edu.appdis.g1.ExamenDelegFernandoServidor.modelo;
+
+import java.util.Date;
+
+public class Ticket {
+	private int id;
+	private int numero;
+	private Date fecha_ingreso;
+	
+	private Vehiculo vehiculo;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public Date getFecha_ingreso() {
+		return fecha_ingreso;
+	}
+
+	public void setFecha_ingreso(Date fecha_ingreso) {
+		this.fecha_ingreso = fecha_ingreso;
+	}
+
+	public Vehiculo getVehiculo() {
+		return vehiculo;
+	}
+
+	public void setVehiculo(Vehiculo vehiculo) {
+		this.vehiculo = vehiculo;
+	}
+	
+	
+}
